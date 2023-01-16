d641c6942ad46a8e060de2f46bde16bcb7663ef3
Anas-Tawalbeh <59394028+Anas-Tawalbeh@users.noreply.github.com>
2023-01-16 09:06:11 +0700

    Create main.py
    Startup

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+Program: Zakat Calculator 
+Author: Anas Tawalbeh
+Terminal-based calcuator that help user calculate the amount of zakat for thier various type of wealth including:
+Cash, Gold, Silver,Agrichulture, Livestock and more
+Significant constants
+         No significant constants
+ 2. The inputs are
+         Type of wealth
+         Amount of wealth
+ 3. Computations:
+         addition: number + another number
+         subtraction: number - another number
+         multiplication: number * another number
+         division: number / another number
+ 4. The outputs are
+         computation result
+"""
+#functions definitions
+def main():
+        #code goes her
+        
+                
+main()
