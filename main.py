45f6c78ad2e2012868952370dc2e365cc3c5b6e0
IkmeeU-daidee <ug642437002@ftu.ac.th>
2023-02-01 23:45:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,35 @@ Significant constants
          zakat amount
 """
 #functions definitions
-def main():
-        print('something')
+#def main():
+        #print('something')
         #code goes her
-        
+
                 
-if __name__ == "__main__":
-         main()
+#if __name__ == "__main__":
+         #main()
+
+
+print("Select choice.")
+print("cash")
+print("gold")
+print("silver")
+
+"cash" >= 200000
+
+while True:
+    # take input from the user
+    choice = input("Enter option: ")
+
+    # check if choice is one of the four options
+    if choice in ('cash', 'gold', 'silver'):
+        try:
+            num1 = float(input("Enter the number: "))
+        except ValueError:
+            print("Invalid input. Please enter the number!!")
+        
+        except:
+            print("Can not calculate!! Please check your data.")
+
+        if choice == 'cash':
+            print(num1*2.5/100)
